40c17ae0ad132244fd0874a4551697705c35ac6e
195882 Pachla Radosław <195882@edu.p.lodz.pl>
2017-11-10 10:29:14 +0100

    start of A*

--- src/fifteensolver/PuzzleCreator.java
+++ src/fifteensolver/PuzzleCreator.java
@@ -16,48 +16,48 @@ public final class PuzzleCreator {
 
     public Tile[] puzzle = new Tile[16]; // to raczej bedziemy zmieniac takze nie final
     private ArrayList<Integer> numbersHolder = new ArrayList(16);
-    private final int x = 4;
-    private final int y = 4;
+    int cost = 0;
+    String listOfMovestoState;
 
     public PuzzleCreator() {
-       // shuffleNumbers();
+        // shuffleNumbers();
         int counter2 = -1;
-       // int counter = 0;
+        // int counter = 0;
         for (int i = 0; i < 16; i++) {
             if (i % 4 == 0) {
                 counter2++;
             }
-            puzzle[i] = new Tile(i - counter2 * 4, counter2,i);
+            puzzle[i] = new Tile(i - counter2 * 4, counter2, i);
             //System.out.println("X: " + puzzle[i].getX() + " " + "Y: " + puzzle[i].getY() + " " + "Val: " + puzzle[i].getValue() + " ");
-         //   counter++;
+            //   counter++;
         }
     }
-    public void ShuffleXTimes(int X)
-    {
+
+    public void ShuffleXTimes(int X) {
         int zer = 0;
         int move = 0;
-        for(int i =0;i<X;i++){
-        do{
-        move = (int)(Math.random()*4);
+        for (int i = 0; i < X; i++) {
+            do {
+                move = (int) (Math.random() * 4);
+            } while (!FifteenSolver.isMoveLegal(move, zer));
+            FifteenSolver.moveTile(zer, move, this);
+            zer = FifteenSolver.zero(this);
         }
-        while(!FifteenSolver.isMoveLegal(move, zer));
-        FifteenSolver.moveTile(zer, move, this);
-        zer = FifteenSolver.zero(this);
-        }
-      
-        
+
     }
 
     public PuzzleCreator(PuzzleCreator auxiliaryPuzzle) {
-       
+
         int counter2 = -1;
-     
+        this.cost = auxiliaryPuzzle.cost;
+        this.listOfMovestoState = auxiliaryPuzzle.listOfMovestoState;
+
         for (int i = 0; i < 16; i++) {
             if (i % 4 == 0) {
                 counter2++;
             }
             puzzle[i] = new Tile(i - counter2 * 4, counter2, 0);
-          
+
         }
         for (int i = 0; i < 16; i++) {
             this.puzzle[i].setValue(auxiliaryPuzzle.puzzle[i].getValue());
@@ -75,7 +75,7 @@ public final class PuzzleCreator {
     public void PrintPuzzle() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                System.out.print(puzzle[i*4 + j ].getValue() + " ");
+                System.out.print(puzzle[i * 4 + j].getValue() + " ");
             }
             System.out.println("");
         }
